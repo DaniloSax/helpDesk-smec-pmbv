b1e79cf9c5b999566fa305303121b8198d9b3a2d
DaniloSax <danilovsdanilo@gmail.com>
2020-10-07 10:02:44 -0400

    [FIX] basURL to server PMBV

--- front/src/views/orders_services/OrderEdit.vue
+++ front/src/views/orders_services/OrderEdit.vue
@@ -99,8 +99,8 @@ export default {
     return {
       newCall: null,
       images: [],
-      baseURL: "http://localhost:8000/storage/",
-      // baseURL: "http://192.168.8.81:8008/storage/",
+      // baseURL: "http://localhost:8000/storage/",
+      baseURL: "http://192.168.8.81:8008/storage/",
       responses: null,
       loading: false,
     };
