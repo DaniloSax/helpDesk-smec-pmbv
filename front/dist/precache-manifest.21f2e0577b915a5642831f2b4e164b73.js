38f8fee138b87d696726eb5fe653631246a64026
DaniloSax <danilovsdanilo@gmail.com>
2020-11-10 13:22:38 -0400

    [ADD] build

---
+++ front/dist/precache-manifest.21f2e0577b915a5642831f2b4e164b73.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "012e7124b3dbeb18411b",
+    "url": "/css/app.b28bd5e5.css"
+  },
+  {
+    "revision": "599c54f178e9b60a73f0",
+    "url": "/css/chunk-vendors.a317322b.css"
+  },
+  {
+    "revision": "2ac63345908b3b6d83b21c333b8df16b",
+    "url": "/img/background_solversStar.2ac63345.jpg"
+  },
+  {
+    "revision": "7aaa9691248486682948d20575f3778b",
+    "url": "/img/confuso.7aaa9691.png"
+  },
+  {
+    "revision": "9f4fd0531a35ce376fc17b98841b03d4",
+    "url": "/img/criar_servico.9f4fd053.gif"
+  },
+  {
+    "revision": "764c6194390652c8fd24c8ba86aceae0",
+    "url": "/img/feliz.764c6194.png"
+  },
+  {
+    "revision": "a98fe52023baf932e241aa54412758b6",
+    "url": "/img/feliz_demais.a98fe520.png"
+  },
+  {
+    "revision": "3b3d3484d8adea88358e3a0aad8209e4",
+    "url": "/img/piscadela.3b3d3484.png"
+  },
+  {
+    "revision": "928b603d6c7e41e6f1e205d7fbd23494",
+    "url": "/img/profile.928b603d.png"
+  },
+  {
+    "revision": "30dd9aed8da89228e609a60e6ba8d56a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "012e7124b3dbeb18411b",
+    "url": "/js/app.f5041415.js"
+  },
+  {
+    "revision": "599c54f178e9b60a73f0",
+    "url": "/js/chunk-vendors.acb24681.js"
+  },
+  {
+    "revision": "697703d83069aa5d602796c7d4949b32",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
